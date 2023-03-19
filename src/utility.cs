eee4f9382827be1055e4fac3e3adafec8df1055c
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-03-19 10:53:52 +0700

    Feat : Utility

--- src/utility.cs
+++ src/utility.cs
@@ -1,1 +1,55 @@
-    
+using System;
+namespace Utility{
+    class Utility{
+        private string[,] matrix;
+
+        public Utility(){
+            matrix = null;
+        }
+
+        public Utility(string[,] matrix){
+            this.matrix = matrix;
+        }
+
+        public void setMatrix(string[,] matrix){
+            this.matrix = matrix;
+        }
+
+        public string[,] getMatrix(){
+            return matrix;
+        }
+
+        public void parserFile(string path){
+            string[] lines = System.IO.File.ReadAllLines(path);
+            int row = lines.Length;
+            int col = lines[0].Split(' ').Length;
+            matrix = new string[row,col];
+            for(int i = 0; i < row; i++){
+                string[] line = lines[i].Split(' ');
+                for(int j = 0; j < col; j++){
+                    matrix[i,j] = line[j];
+                }
+            }
+        }
+
+        public void printMatrix(){
+
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // public static void Main(string[] args){
+        //     Utility utility = new Utility();
+        //     utility.parserFile("../test/input.txt");
+        //     utility.printMatrix();
+
+        // }
+    }
+}
