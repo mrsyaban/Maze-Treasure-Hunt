fffb5ff28d8d13d92400a998a5a7866cddf45714
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-03-19 09:50:23 +0700

    Fix: Tile.cs

--- src/Tile.cs
+++ src/Tile.cs
@@ -7,11 +7,14 @@ namespace Tile
 {
     class Tile
     {
-        int[] coordinate;
-        int value;
-        bool visited;
-        List<Tile> path;
-
+        private int[] coordinate;
+        private int value;
+        private bool visited;
+        private List<Tile> path;
+        private Tile Down;
+        private Tile Right;
+        private Tile Up;
+        private Tile Left;
         /* Constructor */
         public Tile() {
             coordinate = new int[2];
@@ -37,7 +40,18 @@ namespace Tile
         public int[] getCoordinate(){
             return coordinate;
         }
-
+        public Tile getDown(){
+            return Down;
+        }
+        public Tile getRight(){
+            return Right;
+        }
+        public Tile getUp(){
+            return Up;
+        }
+        public Tile getLeft(){
+            return Left;
+        }
 
         /* Setter */
         public void setCoordinate(int[] coor){
@@ -50,7 +64,37 @@ namespace Tile
         }
 
         public void addPath(Tile tile){
-
+            path.Add(tile);
         }
+        public void setValue(int val){
+            value = val;
+        }
+        public void setDown(Tile tile){
+            Down = tile;
+        }
+        public void setRight(Tile tile){
+            Right = tile;
+        }
+        public void setUp(Tile tile){
+            Up = tile;
+        }
+        public void setLeft(Tile tile){
+            Left = tile;
+        }
+        public void printInfo(){
+            Console.WriteLine("Coordinate: " + coordinate[0] + "," + coordinate[1]);
+            Console.WriteLine("Value: " + value);
+            Console.WriteLine("Visited: " + visited);
+            Console.WriteLine("Path: ");
+            foreach(Tile tile in path){
+                Console.WriteLine(tile.getCoordinate()[0] + "," + tile.getCoordinate()[1]);
+            }
+            Console.WriteLine("Neighboor:");
+            Console.WriteLine(Left.getCoordinate()[0] + "," + Left.getCoordinate()[1]);
+            Console.WriteLine(Up.getCoordinate()[0] + "," + Up.getCoordinate()[1]);
+            Console.WriteLine(Right.getCoordinate()[0] + "," + Right.getCoordinate()[1]);
+            Console.WriteLine(Down.getCoordinate()[0] + "," + Down.getCoordinate()[1]);
+        }
+
     }
 }
