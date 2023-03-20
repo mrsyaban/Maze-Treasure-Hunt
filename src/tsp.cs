2206ef6ce9ee1299efc0a7d55cfd41a35eda9a15
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-03-20 16:39:48 +0700

    Feat:Tsp.cs

---
+++ src/tsp.cs
@@ -0,0 +1,37 @@
+using bfs = bfsSpace.bfsClass;
+using Tile = TileSpace.TileClass;
+using Utility = UtilitySpace.Matrix;
+
+namespace tspSpace{
+    public class tspBfs : bfs {
+        private Tile home;
+        public tspBfs(string[,] matrix) : base(matrix) {
+            home = start;
+            // Do nothing
+        }
+        public void setHome(Tile home){
+            this.home = home;
+        }
+        public Tile getHome(){
+            return home;
+        }
+        public void backtoHome(){
+           this.treasure.Add(home);
+           this.BFS();
+        }
+        public void startTsp(){
+            this.startfind();
+            this.backtoHome();
+        }
+
+        public static void Main(string[] args){
+            Utility utility = new Utility();
+            utility.parserFile("../test/input.txt");
+            utility.printMatrix();
+            tspBfs tspBfs = new tspBfs(utility.getMatrix());
+            tspBfs.startTsp();
+            tspBfs.printStep();
+        }
+
+    }
+}
