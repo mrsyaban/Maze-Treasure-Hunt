f2ed52d09530d102f6fc7bdba7931a3cf20c37e5
Naufal-Nalendra <16521240@std.stei.itb.ac.id>
2023-03-23 13:36:31 +0700

    finalize GUI

--- src/Tubes2_BingChilling/MainWindow.xaml.cs
+++ src/Tubes2_BingChilling/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ using System.Runtime.InteropServices.ComTypes;
 
 using Services;
 using System.Reflection;
+using System.Windows.Threading;
 
 namespace Tubes2_BingChilling
 {
@@ -36,206 +37,224 @@ namespace Tubes2_BingChilling
         public MainWindow()
         {
             InitializeComponent();
-
-            // binding to grid in xaml
-            ////this.Maze = Find
-            //Maze.RowDefinitions.Add(new RowDefinition());
-            //Maze.RowDefinitions.Add(new RowDefinition());
-            //var cell = new Grid();
-            //cell.SetValue(Label.ContentProperty, " testtt");
-            //cell.SetValue(Grid.RowProperty, 0);
-            //cell.SetValue(Grid.ColumnProperty, 0);
-            //cell.SetValue(BackgroundProperty, Brushes.White);
-            
-            //Maze.Children.Add(cell);
-        }
-        private void initializeMaze(object sender, EventArgs e)
-        {
-            Window mainWindow = Application.Current.MainWindow;
-            double width = mainWindow.ActualWidth;
-            double height = mainWindow.ActualHeight;
-
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
 
         private void Browse(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Text files (*.txt)|*.txt";
             openFileDialog.Multiselect = false;
+            
             if (openFileDialog.ShowDialog() == true)
             {
 
                 string filePath = openFileDialog.FileName;
+                // set the file path
                 this.pathFile = filePath;
+                FileName.Text = System.IO.Path.GetFileName(filePath);
                 if (System.IO.Path.GetExtension(filePath) != ".txt")
                 {
-                    MessageBox.Show("Please select a .txt file.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show("Please select a .txt file.", "Invalid File Extension", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
                 }
-                else
-                {
-                    m = new Maze(filePath);
-                    for (int i = 0; i < this.m.Width; i++)
-                    {
-                        mazeGrid.RowDefinitions.Add(new RowDefinition());
-                    }
-                    for (int i = 0; i < this.m.Length; i++)
-                    {
-                        mazeGrid.ColumnDefinitions.Add(new ColumnDefinition());
-                    }
-                    TextBox cell;
-                    for (int i = 0; i < this.m.Width; i++)
-                    {
-                        for (int j = 0; j < this.m.Length; j++)
-                        {
-                            cell = new TextBox();
-                            SolidColorBrush brush;
-                            if (this.m.Content[i][j] == "X")
-                            {
-                                brush = new SolidColorBrush(Colors.Black);
-                            }
-                            else if (this.m.Content[i][j] == "K")
-                            {
-                                brush = new SolidColorBrush(Colors.Red);
-                            }
-                            else if (this.m.Content[i][j] == "T")
-                            {
-                                brush = new SolidColorBrush(Colors.Gold);
-                            }
-                            else
-                            {
-                                brush = new SolidColorBrush(Colors.White);
-                            }
-                            cell.Background = brush;
-                            Grid.SetColumn(cell, j);
-                            Grid.SetRow(cell, i);
-                            mazeGrid.Children.Add(cell);
-                        }
-                    }
 
+            }
+        }
+        private void Visualize(object sender, RoutedEventArgs e)
+        {
+            // Clear maze grid
+            mazeGrid.RowDefinitions.Clear();
+            mazeGrid.ColumnDefinitions.Clear();
+
+            // Check that there is an input file
+            if (string.IsNullOrEmpty(this.pathFile))
+            {
+                MessageBox.Show("Please select a file", "No File is Selected", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+
+            }
+
+            // Read the input file
+            string[] lines = File.ReadAllLines(this.pathFile);
+
+            // Concatenate all the lines into a single string and remove any spaces
+            string content = string.Concat(lines).Replace(" ", "");
+
+            // Check that the input file contains only the allowed characters
+            string validChars = "KTRX";
+            if (!content.All(c => validChars.Contains(c)))
+            {
+                MessageBox.Show("Invalid input file. Make sure the file only contains the characters: K, T, R, and X.", "Invalid characters", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Initialize maze grid
+            m = new Maze(this.pathFile);
+
+            for (int i = 0; i < this.m.Width; i++)
+            {
+                mazeGrid.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int i = 0; i < this.m.Length; i++)
+            {
+                mazeGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            TextBox cell;
+            for (int i = 0; i < this.m.Width; i++)
+            {
+                for (int j = 0; j < this.m.Length; j++)
+                {
+                    cell = new TextBox();
+                    SolidColorBrush brush;
+                    if (this.m.Content[i][j] == "X")
+                    {
+                        brush = new SolidColorBrush(Colors.Black);
+                    }
+                    else if (this.m.Content[i][j] == "K")
+                    {
+                        brush = new SolidColorBrush(Colors.Red);
+                    }
+                    else if (this.m.Content[i][j] == "T")
+                    {
+                        brush = new SolidColorBrush(Colors.Gold);
+                    }
+                    else
+                    {
+                        brush = new SolidColorBrush(Colors.White);
+                    }
+                    cell.Background = brush;
+                    Grid.SetColumn(cell, j);
+                    Grid.SetRow(cell, i);
+                    mazeGrid.Children.Add(cell);
                 }
             }
         }
-        private void Start(object sender, RoutedEventArgs e) 
-        {
-            int nodes = 0;
 
-            // Start the Stopwatch
+        private async void Start(object sender, RoutedEventArgs e)
+        {
+            // Check that there is an input file
+            if (string.IsNullOrEmpty(this.pathFile))
+            {
+                MessageBox.Show("Please select a file", "No File is Selected", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+
+            }
+
+            // Set up variables and objects
+            int nodes = 0;
+            List<Tuple<string, int, int, int>> resultPath;
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            List<Tuple<string, int, int, int>> resultPath;
-
-
-            // BFS search with TSP
+            // Check which algorithm was selected
             if (bfsButton.IsChecked == true && tspButton.IsChecked == true)
             {
-                // Set up TSP algorithm
+                BFS bfs = new BFS(this.pathFile);
+
+                // Execute the search algorithm on a separate thread using Task.Run()
+                await Task.Run(() => bfs.runTSP());
+                resultPath = bfs.getResultPath();
+
             }
-            // DFS search with TSP
-            else if (bfsButton.IsChecked == true && tspButton.IsChecked == true)
+            else if (dfsButton.IsChecked == true && tspButton.IsChecked == true)
             {
-                // Set up TSP algorithm
+                DFS dfs = new DFS(this.pathFile);
+
+                // Execute the search algorithm on a separate thread using Task.Run()
+                await Task.Run(() => dfs.runTSP());
+                resultPath = dfs.getResultPath();
+
             }
             else if (bfsButton.IsChecked == true)
             {
                 BFS bfs = new BFS(this.pathFile);
-                bfs.run();
 
+                // Execute the search algorithm on a separate thread using Task.Run()
+                await Task.Run(() => bfs.run());
                 resultPath = bfs.getResultPath();
-                for (int i = 0; i < resultPath.Count; i++)
-                {
-                    routeBox.Text += resultPath[i].ToString();
-
-                }
 
             }
-            // DFS search
             else if (dfsButton.IsChecked == true)
             {
+                // Similar code as above for DFS algorithm
                 DFS dfs = new DFS(this.pathFile);
-                dfs.run();
 
+                // Execute the search algorithm on a separate thread using Task.Run()
+                await Task.Run(() => dfs.run());
                 resultPath = dfs.getResultPath();
-                for (int i = 0; i < resultPath.Count; i++)
-                {
-                    routeBox.Text += resultPath[i].ToString();
-
-                }
                 
             }
-            // No radio button is checked
             else
             {
-                MessageBox.Show("Please select a desired algorithm ", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Please select a desired algorithm ", "No algorithm is selected", MessageBoxButton.OK, MessageBoxImage.Error);
                 Tuple<string, int, int, int> emptyPath = Tuple.Create("None", 0, 0, 0);
                 List<Tuple<string, int, int, int>> emptyList = new List<Tuple<string, int, int, int>>();
                 emptyList.Add(emptyPath);
                 resultPath = emptyList;
             }
-            TextBox res;
-            int count = 0;
-
-            for (int k = 0; k < resultPath.Count; k++)
+            // Visualize the grid step by step 
+            for (int i = 0; i < resultPath.Count; i++)
             {
-                int row = resultPath[k].Item3;
-                int col = resultPath[k].Item4;
-                count = resultPath[k].Item2;
-                for (int j = 0; j < mazeGrid.ColumnDefinitions.Count; j++)
+                await Task.Delay(500); // introduce a 0.5 second delay between each step
+
+                Tuple<string, int, int, int> currentStep = resultPath[i];
+
+                // Update the UI
+                Dispatcher.Invoke(() =>
                 {
-                    for (int i = 0; i < mazeGrid.RowDefinitions.Count; i++)
+                    int row = currentStep.Item3;
+                    int col = currentStep.Item4;
+                    int count = currentStep.Item2;
+
+                    TextBox res = new TextBox();
+                    SolidColorBrush resBrush;
+
+                    if (count == 1)
                     {
-                        if (i == row && j == col)
-                        {
-                            res = new TextBox();
-                            SolidColorBrush resBrush;
-
-                            if (count == 1)
-                            {
-                                resBrush = new SolidColorBrush(Colors.LightGreen);
-                            }
-                            else if (count == 2)
-                            {
-                                resBrush = new SolidColorBrush(Colors.Green);
-                            }
-                            else if (count == 3)
-                            {
-                                resBrush = new SolidColorBrush(Colors.DarkGreen);
-                            }
-                            else if (count == 4)
-                            {
-                                resBrush = new SolidColorBrush(Colors.DarkOliveGreen);
-                            }
-                            else
-                            {
-                                resBrush = new SolidColorBrush(Colors.ForestGreen);
-                            }
-
-                            Grid.SetColumn(res, col);
-                            Grid.SetRow(res, row);
-                            res.Background = resBrush;
-                            mazeGrid.Children.Add(res);
-
-
-                        }
+                        resBrush = new SolidColorBrush(Colors.LightGreen);
+                    }
+                    else if (count == 2)
+                    {
+                        resBrush = new SolidColorBrush(Colors.Green);
+                    }
+                    else if (count == 3)
+                    {
+                        resBrush = new SolidColorBrush(Colors.DarkGreen);
+                    }
+                    else if (count == 4)
+                    {
+                        resBrush = new SolidColorBrush(Colors.DarkOliveGreen);
+                    }
+                    else
+                    {
+                        resBrush = new SolidColorBrush(Colors.ForestGreen);
                     }
 
-                }
+                    Grid.SetColumn(res, col);
+                    Grid.SetRow(res, row);
+                    res.Background = resBrush;
+                    mazeGrid.Children.Add(res);
+                });
             }
-            // Perform some process
-            showProgress(sender, e);
-
-            // Stop the Stopwatch and output the elapsed time to a TextBox control
+            // Stop the Stopwatch
             stopwatch.Stop();
-
-            timeBox.Text = string.Format("{0} ms", stopwatch.Elapsed.TotalMilliseconds.ToString());
-            stepsBox.Text = resultPath.Count.ToString();
-            nodesBox.Text = nodes.ToString();
+            // convert time into double
+            double timeCount = stopwatch.Elapsed.TotalSeconds;
+            timeBox.Text = string.Format("{0} ms",timeCount);
+            // Output the route to routeBox
+            for (int i = 0; i < resultPath.Count; i++)
+            {
+                routeBox.Text += resultPath[i].ToString();
+            }
+            // Output the number of steps
+            stepsBox.Text = "";
+            // Output the number of nodes;
+            nodesBox.Text = resultPath.Count.ToString();
         }
+
+
+
         private void showProgress(object sender, RoutedEventArgs e)
         {
             // Create a new BackgroundWorker object and set its properties
@@ -265,17 +284,6 @@ namespace Tubes2_BingChilling
         {
             // Update the progress bar value with the progress reported by the worker
             progress.Value = e.ProgressPercentage;
-        }
-
-        private void stepsBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        
-        private void Filename_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
         }
 
     }
